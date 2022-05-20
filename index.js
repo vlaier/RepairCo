1e80f28de73df292c7d61b57dbdcbd9c7ff01d37
Piotr <piotrzielu@gmail.com>
2022-05-20 15:54:29 +0200

    add Grid Section and minor fixes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const additionalInfo = document.getElementById("additional-info");
-const showMore = document.getElementById("show-more-btn");
+const showMore = document.getElementById("show-more");
+const downArrow = document.getElementById("caret-down");
 showMore.addEventListener("click", () => {
+  let p = showMore.children[1];
+  if (p.textContent === "Show More") {
+    p.textContent = "Hide";
+  } else {
+    p.textContent = "Show More";
+  }
   additionalInfo.classList.toggle("hidden");
-  console.log("hidden");
+  downArrow.classList.toggle("rotate");
 });
+
+function rotate(element) {
+  console.log(element);
+  console.log(element.classList);
+}

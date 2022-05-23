f5594de76b7a6dfd2d587835ba25ebf9ed85c513
Piotr <piotrzielu@gmail.com>
2022-05-23 11:43:14 +0200

    Add hamburger menu

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const additionalInfo = document.getElementById("additional-info");
 const showMore = document.getElementById("show-more");
 const downArrow = document.getElementById("caret-down");
+const hamburgerButton = document.getElementById("hamburger-button");
+const navMenu = document.getElementById("nav-items");
+
 showMore.addEventListener("click", () => {
   let p = showMore.children[1];
   if (p.textContent === "Show More") {
@@ -16,3 +19,7 @@ function rotate(element) {
   console.log(element);
   console.log(element.classList);
 }
+
+hamburgerButton.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
